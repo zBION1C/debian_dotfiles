382eda0be3d221936e8cabf70f20e3031a049f8d
zBION1C <nmontana745@gmail.com>
2026-06-09 09:55:59 +0200

    Added Noctalia installation and spawn-at-startup

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+# ================================================== Installing Noctalia ==================================================
+curl -fsSL https://pkg.noctalia.dev/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/noctalia.gpg
+echo "deb [signed-by=/etc/apt/keyrings/noctalia.gpg] https://pkg.noctalia.dev/apt trixie main" | sudo tee /etc/apt/sources.list.d/noctalia.list
+sudo apt update
+sudo apt insatll nocatalia-shell
+# =========================================================================================================================
+
 for f in *; do
   if [ -d $f ]; then
     echo "Installing config for $f in $HOME/.config/$f"
