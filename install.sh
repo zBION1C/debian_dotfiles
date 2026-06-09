3ceed8e1b902d06f69a3768d330e9bb95873008f
zBION1C <nmontana745@gmail.com>
2026-06-09 10:34:03 +0200

    Added sudo for cp command

--- install.sh
+++ install.sh
@@ -13,12 +13,12 @@ sudo apt install noctalia-shell
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 git clone https://github.com/niri-wm/niri.git niri-repo && sudo mv niri-repo /opt/niri
 cd /opt/niri && cargo build --release
-cp /opt/niri/target/release/niri /usr/local/bin
-cp /opt/niri/resources/niri-session /usr/local/bin
-cp /opt/niri/resources/niri.desktop /urs/local/share/wayland-sessions
-cp /opt/niri/resources/niri-portals.conf /urs/local/share/xdg-desktop-portal
-cp /opt/niri/resources/niri.service /etc/systemd/user/
-cp /opt/niri/resources/niri-shutdown.target /etc/systemd/user/
+sudo cp /opt/niri/target/release/niri /usr/local/bin
+sudo cp /opt/niri/resources/niri-session /usr/local/bin
+sudo cp /opt/niri/resources/niri.desktop /urs/local/share/wayland-sessions
+sudo cp /opt/niri/resources/niri-portals.conf /urs/local/share/xdg-desktop-portal
+sudo cp /opt/niri/resources/niri.service /etc/systemd/user/
+sudo cp /opt/niri/resources/niri-shutdown.target /etc/systemd/user/
 # =========================================================================================================================
 
 for f in *; do
