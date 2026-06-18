6dbdac4cbcb5094a940d91070317f2300fccf9af
zBION1C <nmontana745@gmail.com>
2026-06-18 20:48:16 +0200

    Added some old plugins to configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Disable default mappings for vimtex
+vim.g.vimtex_mappings_enabled = 0
+vim.keymap.set("n", "<leader>tc", "<plug>(vimtex-compile)")
